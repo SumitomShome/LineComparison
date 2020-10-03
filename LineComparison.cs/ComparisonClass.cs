ad95e3a084fb9ce9e68307bce6685d843c624643
SumitomShome <sumitomshome@gmail.com>
2020-10-03 18:47:31 +0530

    [Sumitom] Commiting UC4-ComparisonClass

---
+++ LineComparison.cs/ComparisonClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LineCompare
+{
+    public class ComparisonClass
+    {
+        public double CalculateLength()
+        {
+            private double x1, y1, x2, y2;
+            Console.WriteLine("Enter the coordinates for the line");
+            Console.WriteLine("Enter x coordinate of starting point");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y coordinate of the starting point");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x coordinate of the ending point");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y coordinate of the ending point");
+            y2 = Convert.ToDouble(Console.ReadLine());
+            float length = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            return length;
+        }
+        public void CompareLength(double length1, double length2)
+        {
+            int differenceInLength = length1.CompareTo(length2);
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("The lines are of equal length");
+            }
+            else if (differenceInLength > 0)
+            {
+                Console.WriteLine("The 1st line is longer than the 2nd line");
+            }
+            else
+            {
+                Console.WriteLine("The 2nd line is longer than the 1st line");
+            }
+            Console.ReadKey();
+        }
+}
--- LineComparison.cs/Program.cs
+++ LineComparison.cs/Program.cs
@@ -1,45 +1,18 @@
 ﻿using System;
-namespace LineComparison
+using System;
+namespace LineCompare
 {
     class Program
     {
-        static float CalculateLength(double x1, double y1, double x2, double y2)
+       public static void Main(string[] args)
         {
-            float length = (float)Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-            return length;
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
-            float[] length = new float[2];
-            for (int i = 0; i <= 1; i++)
+            ComparisonClass lines = new ComparisonClass();
+            double[] lengthOfLine = new double[2];
+            for (int noOfLine = 0; noOfLine <= 1; noOfLine++)
             {
-                double x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-                Console.WriteLine("Enter the coordinates for the line" +(i+1));
-                Console.WriteLine("Enter x coordinate of starting point");
-                x1 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Enter y coordinate of the starting point");
-                y1 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Enter x coordinate of the ending point");
-                x2 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Enter y coordinate of the ending point");
-                y2 = Convert.ToDouble(Console.ReadLine());
-                length[i] = CalculateLength(x1, y1, x2, y2);
-                Console.WriteLine("The length of the line is: " +length[i]);
+                lengthOfLine[noOfLine] = lines.CalculateLength();
             }
-            int x = length[0].CompareTo(length[1]);
-            if (length[0].Equals(length[1]))
-             {
-                Console.WriteLine("The lines are of equal length");
-            }
-            else if (length[0] > length[1])
-            {
-                Console.WriteLine("The 1st line is longer than the 2nd line");
-            }
-            else
-            {
-                Console.WriteLine("The 2nd line is longer than the 1st line");
-            }
+            lines.CompareLength(lengthOfLine[0], lengthOfLine[1]);
             Console.ReadKey();
         }
     }
